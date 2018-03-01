7fb18be0f1fd27515bb9760b2242cffa43ae37a0
matchli <459044374@qq.com>
2018-03-01 09:45:29 +0800

    新工程

---
+++ git1/git1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git1
+//
+//  Created by lq on 2018/3/1.
+//  Copyright © 2018年 lq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git1/git1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git1
+//
+//  Created by lq on 2018/3/1.
+//  Copyright © 2018年 lq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
